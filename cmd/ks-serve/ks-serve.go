b3552583a556c8635a55ae174f9b3867b4ea749a
David Ressman <davidr@ressman.org>
2018-07-18 16:24:01 -0500

    more WIP

--- cmd/ks-serve/ks-serve.go
+++ cmd/ks-serve/ks-serve.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"flag"
+	"net/http"
 
 	"github.com/davidr/atomic-ks-httpserver/internal/serveks"
 	log "github.com/sirupsen/logrus"
+	"fmt"
 )
 
 func main() {
 	log.SetLevel(log.DebugLevel)
+	log.Debugln("parsing flags")
+
+	flag.StringVar(&serveks.HostFile, "config", "/etc/dnsmasq.conf", "dnsmasq.conf for host definitions")
+	flag.StringVar(&serveks.KsTmplFile, "template", "/srv/kickstart/ks.tmpl", "Kickstart template file")
+	listenIp := flag.String("interface", "127.0.0.1", "IP of interface on which to listen")
+	portNumber := flag.Int("port", 8080, "port number")
+	flag.Parse()
+
 	log.Debugln("starting program")
 
-	hostFile := "examples/dnsmasq.conf.EXAMPLE"
-	ksTmplFile := "examples/node.ks.EXAMPLE"
+	err := serveks.TemplateInit()
+	if err != nil {
+		log.Fatal("Could not initialize template engine: %s", err)
+	}
+	http.HandleFunc("/kickstart", serveks.HttpKSHandler)
 
-	hostsMap, _ := serveks.NewHostsMap(hostFile)
-
-	var t bytes.Buffer
-	serveks.RenderKsFile(&t, hostsMap.Hosts["10.0.0.101"], ksTmplFile)
-	fmt.Println(t.String())
-
+	listenIfaceString := fmt.Sprintf("%s:%d", *listenIp, *portNumber)
+	log.Fatal(http.ListenAndServe(listenIfaceString, nil))
 }
--- internal/serveks/serveks.go
+++ internal/serveks/serveks.go
@@ -9,14 +9,25 @@ import (
 	"text/template"
 
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
+
+
+var HostFile = "examples/dnsmasq.conf.EXAMPLE"
+var KsTmplFile = "examples/node.ks.EXAMPLE"
+
+var HMap HostsMap
+
+var isInitialized = false
 
 // There's really no reason to make this a datatype other than that it's convenient to reference fields
 // when it gets passed to the template engine
 type Host struct {
-	IpAddr   string
-	HostName string
-	macAddr  string
+	IpAddr     string
+	HostName   string
+	Router     string
+	NameServer string
+	macAddr    string
 }
 
 type HostsMap struct {
@@ -27,18 +38,34 @@ type HostsMap struct {
 // NetHostsMap takes a path name to a dnsmasq.conf file and returns a HostsMap of the
 // hosts contained therein keyed by IP (as the http server fills in the template based
 // on the client's IP
-func NewHostsMap(hostFile string) (HostsMap, error) {
-	hm := HostsMap{file: hostFile}
+func NewHostsMap() (HostsMap, error) {
+	hm := HostsMap{file: HostFile}
 	hm.Hosts = make(map[string]Host)
 
-	file, err := os.Open(hostFile)
+	file, err := os.Open(HostFile)
 	if err != nil {
-		log.Warn("Unable to open %s (%s)", hostFile, err)
+		log.Warn("Unable to open %s (%s)", HostFile, err)
 		return hm, err
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+
+	var defaultRouter, nameServer string
+
+	// Do a first scan for the nameserver and router.
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "dhcp-option=option:router") {
+			defaultRouter = strings.Split(line, ",")[1]
+		} else if strings.HasPrefix(line, "dhcp-option=option:dns-server") {
+			nameServer = strings.Split(line, ",")[1]
+		}
+	}
+
+	// reset the position and create a new scanner to go through a second time for hosts
+	file.Seek(0, 0)
+	scanner = bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -52,19 +79,29 @@ func NewHostsMap(hostFile string) (HostsMap, error) {
 				continue
 			}
 
-			hm.Hosts[fields[1]] = Host{IpAddr: fields[1], HostName: fields[2]}
+			hm.Hosts[fields[1]] = Host{IpAddr: fields[1], HostName: fields[2], Router: defaultRouter, NameServer: nameServer}
 		}
 	}
 
 	return hm, nil
 }
 
+func TemplateInit() error {
+	var err error
+	HMap, err = NewHostsMap()
+	if err != nil {
+		return err
+	}
+
+	isInitialized = true
+	return nil
+}
+
 // RenderKsFile generates the text of the kickstart file from the template located in
 // ksTmplFile with the Host data substituted in the template file.
-func RenderKsFile(renderedTmpl *bytes.Buffer, host Host, ksTmplFile string) error {
+func RenderKsFile(renderedTmpl *bytes.Buffer, host Host) error {
 	log.Debugln("starting template render")
-	fmt.Println(host)
-	tmpl, err := template.ParseFiles("examples/node.ks.EXAMPLE")
+	tmpl, err := template.ParseFiles(KsTmplFile)
 	if err != nil {
 		log.Fatalf("could not create template: %s", err)
 	}
@@ -76,4 +113,37 @@ func RenderKsFile(renderedTmpl *bytes.Buffer, host Host, ksTmplFile string) erro
 	}
 
 	return nil
+}
+
+func generateKsConfig(remIpAddr string) (*bytes.Buffer, error) {
+	var buf bytes.Buffer
+
+	host, ok := HMap.Hosts[remIpAddr]
+	if !ok {
+		log.Errorf("host %s not found in dnsmasq config", remIpAddr)
+		return &buf, fmt.Errorf("host %s not found in dnsmasq.conf", remIpAddr)
+	}
+
+	log.Warnf("host: <<%s>>\n", host)
+	err := RenderKsFile(&buf, host)
+	if err != nil {
+		return &buf, err
+	}
+
+	return &buf, nil
+}
+
+func getIPFromRemoteAddr(s string) string {
+	return strings.Split(s, ":")[0]
+}
+
+func HttpKSHandler(w http.ResponseWriter, r *http.Request) {
+	renderedKsTemplate, err := generateKsConfig(getIPFromRemoteAddr(r.RemoteAddr))
+	if err != nil {
+		errstr := fmt.Sprintf("could not render kickstart template: %s", err)
+		http.Error(w, errstr, 500)
+		return
+	}
+
+	fmt.Fprint(w, renderedKsTemplate.String())
 }
